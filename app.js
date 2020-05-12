a74e4f961c323cccf7504d0309a247e65ac2d8dd
Irfan Ezani <irfanezani@gmail.com>
2020-05-13 02:30:21 +0800

    js functionality complete

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const quoteBtn = document.getElementById('quote-btn')
 const quoteContainer = document.getElementById('quote')
 
 quoteBtn.addEventListener('click', () => {
-  fetch("https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=famous&count=10", {
-  	"method": "GET",
-  	"headers": {
-  		"x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com",
-  		"x-rapidapi-key": "6e4a5d2c5emsh95545ce37351636p172b39jsn99f7a93c0f78"
-  	}
-  })
-  .then(response => {
-  	console.log(response);
-  })
-  .catch(err => {
-  	console.log(err);
-  });
+  fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+        getQuote(data)
+    })
+
 })
+
+function getQuote(quote) {
+  quoteContainer.innerHTML = `
+  <div class="main">
+    <h3>${quote.content}</h3>
+    <p>${quote.author}</p>
+
+  </div>`;
+}
